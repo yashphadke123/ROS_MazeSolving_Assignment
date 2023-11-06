fc93aa0552f1971e95c3683eea179e0cbb7de7e5
yashphadke123 <f20211942@goa.bits-pilani.ac.in>
2023-11-06 22:02:14 +0530

    shift to linux

--- maze_solver/src/scripts/path_planner.py
+++ maze_solver/src/scripts/path_planner.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 #Following is the import line for importing the obstacle detection methods i.e. isValidPoint()
-import obstacle_detection as obsdet
+from shapely import Point,LineString
+import matplotlib as plt
+
